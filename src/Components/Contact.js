9bdd794d6eac331369c39520ac9ef08fc162ea9d
csolly <cjaysolly@gmail.com>
2024-05-10 12:45:09 -0400

    changed email address on contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -106,7 +106,7 @@ function Contact() {
                                                     <svg className="flex-shrink-0 text-blue-600 w-7 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                                                     </svg>
-                                                    <span className="block ml-3 text-base font-overpass text-gray-900"> WeHiggins96@yahoo.com</span>
+                                                    <span className="block ml-3 text-base font-overpass text-gray-900"> WeHiggins996@gmail.com</span>
                                                 </div>
 
                                                 <div className="flex items-start">
